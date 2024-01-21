d8e7f94f7f87fb12514addf420e8a901fbba044d
Moyasi Ginko <mrsiyam61@gmail.com>
2024-01-22 03:49:05 +0600

    Update orderSlice.js

--- src/redux/reducers/orderSlice.js
+++ src/redux/reducers/orderSlice.js
@@ -1,6 +1,7 @@
 // ordersSlice.js
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
+import { getToken, getUserId } from '../../utils/userStorage';
 
 const BASE_URL = `${process.env.REACT_APP_API_URL}/order`;
 
@@ -11,26 +12,28 @@ const initialState = {
 
 const createOrder = createAsyncThunk(
   'orders/createOrder',
-  async ({ customerInfo, items, totalPrice }, { getState }) => {
-    const token = JSON.parse(localStorage.getItem('token'));
+  async ({ productList, totalPrice }) => {
+    const token = getToken();
     const headers = {
       'Content-Type': 'application/json',
       Authorization: `Bearer ${token}`,
     };
-    const { userId } = getState().auth; // Assuming you have userId in auth state
+    const userId = getUserId(); // Assuming you have userId in auth state
+
+    // Convert totalPrice to a number
+    const numericTotalPrice = parseFloat(totalPrice);
 
     try {
       const response = await axios.post(
         BASE_URL,
         {
-          customerInfo,
-          items,
-          totalPrice,
+          productList,
+          totalPrice: numericTotalPrice,
           customerId: userId,
         },
         { headers },
       );
-      return response.data;
+      return response.data; // Return the data property of the response
     } catch (error) {
       return Promise.reject(error.response.statusText);
     }
