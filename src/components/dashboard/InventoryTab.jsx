33552fa44e5e14ca045c87ebab27915fac485c18
Moyasi Ginko <mrsiyam61@gmail.com>
2024-01-23 12:41:17 +0600

    Update InventoryTab.jsx

--- src/components/dashboard/InventoryTab.jsx
+++ src/components/dashboard/InventoryTab.jsx
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
 import {
   fetchInventory,
   deleteInventoryItem,
   addInventoryItem,
+  updateInventoryItem,
+  fetchInventoryById,
 } from '../../redux/reducers/inventorySlice';
-import { fetchTrades } from '../../redux/reducers/tradesSlice'; // Import the fetchTrades action
+import { fetchTrades } from '../../redux/reducers/tradesSlice';
 import { getUserId } from '../../utils/userStorage';
 
 const InventoryTab = () => {
@@ -14,18 +15,18 @@ const InventoryTab = () => {
   const [selectedProductId, setSelectedProductId] = useState('');
   const [selectedProductName, setSelectedProductName] = useState('');
   const [quantity, setQuantity] = useState(0);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editingItemId, setEditingItemId] = useState(null);
 
   const dispatch = useDispatch();
   const inventoryItems = useSelector((state) => state.inventory.items);
-  const trades = useSelector((state) => state.trades.trades); // Fetch trades from the trades slice
+  const trades = useSelector((state) => state.trades.trades);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const userId = getUserId();
-        // Fetch all inventory items for the current user (vendor)
         await dispatch(fetchInventory(userId));
-        // Fetch trades and store them in the products array
         await dispatch(fetchTrades());
         setLoading(false);
       } catch (error) {
@@ -39,7 +40,6 @@ const InventoryTab = () => {
 
   const handleDeleteInventory = async (inventoryId) => {
     try {
-      // Delete the inventory item
       await dispatch(deleteInventoryItem(inventoryId));
     } catch (error) {
       console.error('Error deleting inventory item:', error.message);
@@ -47,42 +47,76 @@ const InventoryTab = () => {
   };
 
   const handleProductChange = (selectedProduct) => {
-    // Update state with selected product information
     setSelectedProductId(selectedProduct.id);
     setSelectedProductName(selectedProduct.name);
   };
 
   const handleAddInventory = async () => {
     try {
-      // Create inventory data based on selected product and quantity
       const inventoryData = {
         productId: selectedProductId,
         productName: selectedProductName,
         quantity: quantity,
       };
 
-      // Add the inventory item
       await dispatch(addInventoryItem(inventoryData));
+      // Reset the form after adding
+      setSelectedProductId('');
+      setSelectedProductName('');
+      setQuantity(0);
     } catch (error) {
       console.error('Error adding inventory item:', error.message);
     }
   };
 
-  if (loading) {
-    return <div>Loading...</div>; // Or a loading spinner
-  }
+  const handleEditInventory = (inventoryId) => {
+    // Fetch the inventory item by id
+    dispatch(fetchInventoryById(inventoryId));
+    // Set editing mode and item id
+    setIsEditing(true);
+    setEditingItemId(inventoryId);
+  };
 
-  console.log('Inventory Items:', inventoryItems);
-  console.log('Trades:', trades[3].name);
+  const handleUpdateInventory = async () => {
+    try {
+      const updatedData = {
+        productName: selectedProductName,
+        quantity: quantity,
+      };
+
+      await dispatch(
+        updateInventoryItem({ inventoryId: editingItemId, updatedData })
+      );
+      // Reset the form after updating
+      setIsEditing(false);
+      setEditingItemId(null);
+      setSelectedProductId('');
+      setSelectedProductName('');
+      setQuantity(0);
+    } catch (error) {
+      console.error('Error updating inventory item:', error.message);
+    }
+  };
+
+  const handleCancelEdit = () => {
+    // Reset editing state
+    setIsEditing(false);
+    setEditingItemId(null);
+    setSelectedProductId('');
+    setSelectedProductName('');
+    setQuantity(0);
+  };
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div>
-      {/* Dropdown to select a product */}
       <select onChange={(e) => handleProductChange(JSON.parse(e.target.value))}>
         <option value="" disabled selected>
           Select a Product
         </option>
-        {/* Map through trades to create options */}
         {trades.map((trade) => (
           <option
             key={trade.id}
@@ -93,7 +127,6 @@ const InventoryTab = () => {
         ))}
       </select>
 
-      {/* Input for quantity */}
       <input
         type="number"
         value={quantity}
@@ -101,12 +134,21 @@ const InventoryTab = () => {
         placeholder="Quantity"
       />
 
-      {/* Button to add inventory */}
-      <button type="button" onClick={handleAddInventory}>
-        Add Inventory
-      </button>
+      {isEditing ? (
+        <div>
+          <button type="button" onClick={handleUpdateInventory}>
+            Save
+          </button>
+          <button type="button" onClick={handleCancelEdit}>
+            Cancel
+          </button>
+        </div>
+      ) : (
+        <button type="button" onClick={handleAddInventory}>
+          Add Inventory
+        </button>
+      )}
 
-      {/* Display existing inventory items */}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {inventoryItems.map((inventoryItem) => (
           <div key={inventoryItem.id} className="border p-4 rounded-lg">
@@ -114,21 +156,39 @@ const InventoryTab = () => {
               {inventoryItem.productName}
             </h2>
             <p className="text-gray-500 mb-2">{`Quantity: ${inventoryItem.quantity}`}</p>
-            {/* Link to the edit page with the inventory item ID as a parameter */}
-            <Link
-              to={`/inventory/edit/${inventoryItem.id}`}
-              className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
-            >
-              Edit
-            </Link>
-            {/* Button to delete the inventory item */}
-            <button
-              type="button"
-              onClick={() => handleDeleteInventory(inventoryItem.id)}
-              className="bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600 ml-2"
-            >
-              Delete
-            </button>
+
+            {isEditing && editingItemId === inventoryItem.id ? (
+              <div>
+                {/* Editing controls */}
+                <button
+                  type="button"
+                  onClick={() => handleUpdateInventory(inventoryItem.id)}
+                >
+                  Save
+                </button>
+                <button type="button" onClick={handleCancelEdit}>
+                  Cancel
+                </button>
+              </div>
+            ) : (
+              <div>
+                {/* Non-editing controls */}
+                <button
+                  type="button"
+                  onClick={() => handleEditInventory(inventoryItem.id)}
+                  className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
+                >
+                  Edit
+                </button>
+                <button
+                  type="button"
+                  onClick={() => handleDeleteInventory(inventoryItem.id)}
+                  className="bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600 ml-2"
+                >
+                  Delete
+                </button>
+              </div>
+            )}
           </div>
         ))}
       </div>
