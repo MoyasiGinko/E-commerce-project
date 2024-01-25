aee406252dfe9e49078b1640435fb33b09f4ae5b
Moyasi Ginko <mrsiyam61@gmail.com>
2024-01-25 22:02:22 +0600

    Update splash screen design

--- src/components/SplashScreen.jsx
+++ src/components/SplashScreen.jsx
@@ -3,6 +3,7 @@ import { useNavigate, Link } from 'react-router-dom';
 import backgroundImage from '../assets/images/bg-ecom-4.jpg';
 import cartImage from '../assets/images/cart1.gif';
 import { getUserData } from '../utils/userStorage';
+// import './SplashScreen.css'; // Import the CSS file for additional styling
 
 const SplashScreen = () => {
   const navigate = useNavigate();
@@ -25,9 +26,10 @@ const SplashScreen = () => {
   return (
     <div className="relative h-screen overflow-hidden">
       {/* Navbar */}
-      <nav className="fixed top-0 left-0 right-0 z-10 flex justify-between items-center p-4 bg-gray-800">
-        {/* Logo in the middle */}
+      <nav className="fixed top-0 left-0 right-0 z-10 flex justify-between items-center p-2 px-12 bg-gray-900">
+        {/* Logo on the left */}
         <div className="flex items-center space-x-4">
+          <img src={cartImage} alt="Logo" className="w-12 h-12" />
           <Link to="/" className="text-white text-2xl font-bold">
             MicroCommerce
           </Link>
@@ -38,14 +40,14 @@ const SplashScreen = () => {
           <button
             type="button"
             onClick={handleSignup}
-            className="text-white hover:text-gray-300 focus:outline-none"
+            className="text-white font-bold hover:text-red-600 focus:outline-none transition duration-300"
           >
             Sign Up
           </button>
           <button
             type="button"
             onClick={handleLogin}
-            className="text-white hover:text-gray-300 focus:outline-none"
+            className="text-white font-bold hover:text-blue-600 focus:outline-none transition duration-300"
           >
             Log In
           </button>
@@ -61,14 +63,12 @@ const SplashScreen = () => {
           backgroundPosition: 'center',
         }}
       >
-        <div className="bg-white bg-opacity-10 p-12 text-center rounded-full shadow-lg mb-8">
-          <div className="flex justify-center items-center mb-4">
-            <img src={cartImage} alt="Loading Animation" className="w-60" />
-          </div>
+        <div className="text-container">
+          <img src={cartImage} alt="Loading Animation" className="w-40 h-40" />
           <h1 className="text-4xl font-bold text-white mb-4 font-merriweather">
             Welcome to MicroCommerce
           </h1>
-          <p className="text-lg font-bold text-accent text-white mb-6 font-open-sans">
+          <p className="text-lg font-bold text-accent text-white font-open-sans">
             Explore a world of possibilities for customers.
           </p>
         </div>
