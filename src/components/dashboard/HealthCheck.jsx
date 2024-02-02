e56ed6c047037c7919442701a008657b1e742aba
Moyasi Ginko <mrsiyam61@gmail.com>
2024-02-03 03:47:53 +0600

    Update HealthCheck.jsx

--- src/components/dashboard/HealthCheck.jsx
+++ src/components/dashboard/HealthCheck.jsx
@@ -9,13 +9,13 @@ class HealthComponent extends Component {
     };
   }
 
-  // componentDidMount() {
-  //   // Fetch health data when the component mounts
-  //   fetch('http://localhost:3001/index.html') // Use the URL of your health endpoint http://localhost:8761
-  //     .then((response) => response.text()) // Use response.text() instead of response.json()
-  //     .then((data) => this.setState({ healthData: data }))
-  //     .catch((error) => this.setState({ error }));
-  // }
+  componentDidMount() {
+    // Fetch health data when the component mounts
+    fetch('http://localhost:3001/index.html') // Use the URL of your health endpoint http://localhost:8761
+      .then((response) => response.text()) // Use response.text() instead of response.json()
+      .then((data) => this.setState({ healthData: data }))
+      .catch((error) => this.setState({ error }));
+  }
 
   handleButtonClick = () => {
     // Open the link when the button is clicked
@@ -34,11 +34,7 @@ class HealthComponent extends Component {
           }}
         >
           <div>
-            <span>
-              Check your applications health. If the page does not render,
-              please
-              {' '}
-            </span>
+            <span>If the page does not render, </span>
             <button
               className="text-red-500"
               type="button"
@@ -62,11 +58,7 @@ class HealthComponent extends Component {
           }}
         >
           <div>
-            <span>
-              Check your applications health. If the page does not render,
-              please
-              {' '}
-            </span>
+            <span>If the page does not render, </span>
             <button
               className="text-red-500"
               type="button"
@@ -81,12 +73,67 @@ class HealthComponent extends Component {
     }
 
     return (
-      <div>
+      <div
+        style={{
+          backgroundColor: 'rgba(255, 255, 255, 0.1)',
+          color: 'rgba(25, 25, 255, 1)',
+        }}
+      >
+        <style>
+          {`
+            body {
+              font-family: 'Arial', sans-serif;
+              margin: 0;
+              padding: 0;
+              background-color: rgba(255, 255, 255, 0.1); /* Set background color here */
+            }
+
+            .container {
+              max-width: 1200px;
+              margin: 0 auto;
+              padding: 20px;
+            }
+
+            h1 {
+              color: white; /* Set text color here */
+              margin-bottom: 20px;
+            }
+
+            table {
+              width: 100%;
+              margin-bottom: 20px;
+              border-collapse: collapse;
+              border-spacing: 0;
+            }
+
+            th, td {
+              padding: 8px;
+              text-align: left;
+              border-bottom: 1px solid  rgba(255, 255, 255, 0.1);
+            }
+
+            th {
+              background-color:  rgba(255, 255, 255, 0.1);
+            }
+
+            .navbar {
+              background-color:  rgba(255, 255, 255, 0.1);
+              padding: 10px 0;
+            }
+
+            .navbar a {
+              color: white;
+              text-decoration: none;
+              margin: 0 15px;
+            }
+
+            .navbar a:hover {
+              text-decoration: underline;
+            }
+          `}
+        </style>
         <div>
-          <span>
-            Check your applications health. If the page does not render, please
-            {' '}
-          </span>
+          <span>If the page does not render, </span>
           <button
             className="text-red-500"
             type="button"
@@ -95,6 +142,7 @@ class HealthComponent extends Component {
             click here
           </button>
         </div>
+        <div dangerouslySetInnerHTML={{ __html: healthData }} />
       </div>
     );
   }
